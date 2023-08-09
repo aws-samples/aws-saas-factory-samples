17b8db11babeba82199ba97c64755b98173acd80
Alex Pulver <apulver@amazon.com>
2023-08-09 07:28:24 +0300

    saas-tenant-isolation-abac-jwt-sts

---
+++ saas-tenant-isolation-abac-jwt-sts/auth0_client.py
@@ -0,0 +1,26 @@
+import os
+
+from auth0.management import Auth0
+
+import constants
+
+
+def main() -> None:
+    mgmt_api_token = os.environ["AUTH0_MGMT_API_TOKEN"]
+    auth0 = Auth0(constants.OIDC_PROVIDER_DOMAIN, mgmt_api_token)
+
+    client = auth0.clients.create(  # type: ignore[attr-defined]
+        {
+            "name": "UI",
+            "callbacks": ["https://localhost"],
+            "grant_types": ["implicit"],
+            "jwt_configuration": {
+                "alg": "RS256",
+            },
+        }
+    )
+    print(client["client_id"])
+
+
+if __name__ == "__main__":
+    main()
